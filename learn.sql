883950edacad00db4476131e05d1838c3819e0ad
TheRogue <36058852+rogue7373@users.noreply.github.com>
2022-01-16 20:08:35 -0700

    Added CREATE TABLE and Various JOIN Examples

--- learn.sql
+++ learn.sql
@@ -78,3 +78,67 @@ SELECT DepartmentName, EmployeeName
 FROM Department
 LEFT JOIN Employee
 ON Departmnet.Manager = Employee.ID;
+
+-- Equijoin compares columns of two tables with the = operator. Most joins are equijoins. --
+SELECT Name, Address
+FROM Buyer 
+LEFT JOIN Property
+ON Price = MaxPrice;
+
+
+-- Non-Equijoin compares columns with an opeerator other than =, such as <, >.  --
+SELECT Name, Address
+FROM Buyer
+LEFT JOIN Property
+ON Price < MaxPrice;
+
+-- Self Join Example --
+SELECT A.Name, B.Name
+FROM EmployeeManager A 
+INNER JOIN EmployeeManager B 
+ON B.ID = A.Manager;
+
+-- Cross Join Example --
+SELECT Model, Gigabytes, IPhone.Price + Memory.Price
+FROM IPhone
+CROSS JOIN Memory;
+
+-- Working with Joins, this snippet you can change the SELECT Statement to LEFT, RIGHT, INNER, or CROSS JOINS to see the behavior -- 
+CREATE TABLE Genre (
+  Code CHAR(3),
+  Name VARCHAR(20),
+  Description VARCHAR(200),
+  PRIMARY KEY(Code)
+);
+
+CREATE TABLE Song (
+  ID INT,
+  Title VARCHAR(60),
+  Artist VARCHAR(60),
+  Code CHAR(3),
+  PRIMARY KEY (ID),
+  FOREIGN KEY Song(Code) REFERENCES Genre(Code)
+);
+
+INSERT INTO Genre VALUES
+  ('CLA', 'Classical', 'Orchestral music composed and performed by professionally trained artists'),
+  ('COU', 'Country', 'Developed mostly in southern USA, with roots in traditional folk music, spirituals and blues'),
+  ('DRO', 'Drone', 'Minimalist music that emphasizes sustained or repeated sounds, notes, or tone clusters'),
+  ('GRU', 'Grunge', 'Alternative rock inspired by hardcore punk, heavy metal, and indie rock'),
+  ('PRC', 'Pop Rock', 'Rock music with less attitude'),
+  ('RAB', 'R&B', 'Modern version of soul and funk African-American pop music'),
+  ('TEC', 'Techno', 'Electronic music');
+
+INSERT INTO Song VALUES
+  (100, 'Hey Jude', 'Beatles', 'PRC'),
+  (200, 'You Belong With Me', 'Taylor Swift', NULL),
+  (300, 'Need You Now', 'Lady Antebellum', 'COU'),
+  (400, 'Old Town Road', 'Lil Nas X', NULL),
+  (500, 'That\'s The Way Love Goes', 'Janet Jackson', 'RAB'),
+  (600, 'Even Flow', 'Pearl Jam', 'GRU');
+
+
+SELECT *
+FROM Song
+CROSS JOIN Genre
+ON Song.Code = Genre.Code;
