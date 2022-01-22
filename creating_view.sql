44cd54ac00cb5943787677ad29c6331ccadfbbad
TheRogue <36058852+rogue7373@users.noreply.github.com>
2022-01-21 21:08:43 -0700

    Added View Samples update

--- creating_view.sql
+++ creating_view.sql
@@ -3,3 +3,14 @@
 -- Example of Creating a View of Table in MySQL --
 CREATE VIEW ViewName [ ( Column1, Column2, ...) ]
 AS SelectStatement;
+
+-- Example of Full View Statement --
+CREATE VIEW ManagerView 
+AS SELECT DepartmentName, EmployeeName AS ManagerName
+    FROM Department, Employee
+    WHERE ManagerID = EmployeeID;
+
+-- Example of View --
+CREATE VIEW ViewName [ ( Column1, Column2, ...)]
+AS SelectStatement
+[   WITH CHECK OPTION ];
